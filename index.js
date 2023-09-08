f069e15619f81f68de0d185bf4b86db08a3d3ac1
Adebowale Joseph <joekamix320@gmail.com>
2023-09-08 04:20:13 +0100

    modified time to show in milliseconds

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const time = document.querySelector(".time");
 
 const dayTimeOftheWeek = new Date()
 let d = dayTimeOftheWeek.getDay()
-let hours = dayTimeOftheWeek.getHours()
-let minutes = dayTimeOftheWeek.getMinutes()
-let seconds = dayTimeOftheWeek.getSeconds()
+// let hours = dayTimeOftheWeek.getHours()
+// let minutes = dayTimeOftheWeek.getMinutes()
+// let seconds = dayTimeOftheWeek.getSeconds()
+let milliSeconds = dayTimeOftheWeek.getTime()
 
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 day.textContent = days[d - 1]
-time.textContent = `${hours} : ${minutes} : ${seconds}`
+time.textContent = `${milliSeconds}`
 
